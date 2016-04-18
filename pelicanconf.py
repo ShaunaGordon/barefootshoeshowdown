71bd4d3060429b2c8626c178b4cdd3130440bd87
Shauna Gordon <shauna@shaunagordon.com>
2016-04-18 14:07:08 -0400

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Shauna Gordon'
+SITENAME = 'Barefoot Shoe Showdown'
+SITEURL = 'http://shoes.shaunagordon.com'
+
+PATH = 'content'
+
+ARTICLE_PATHS = ['posts']
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+TRANSLATION_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+# Delete the contents of the output directory...
+DELETE_OUTPUT_DIRECTORY = False
+
+# ...but save the git stuff
+OUTPUT_RETENTION = ('.git', '.gitignore')
+
+# Plugins
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['clean_summary', 'collate_content', 'feed_summary', 'footer_insert', 'extract_toc', 'related_posts', 'simple_footnotes', 'share_post', 'summary', 'disqus_static', 'liquid_tags.img','liquid_tags.include_code']
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
+
+# Theme
+THEME = 'twenty'
+
+# Theme Settings
+DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+STATIC_PATHS = ['theme/images', 'images']
+TAG_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+# Blogroll
+LINKS = ()
+
+# Social widgets
+SOCIAL = ()
+
+TWITTER_USERNAME = ''
+
+# Comments
+DISQUS_SITENAME = ''
+DISQUS_SECRET_KEY = ''
+DISQUS_PUBLIC_KEY = ''
+
+DEFAULT_PAGINATION = 10
+
+DISPLAY_PAGES_ON_MENU = True
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = False
+
+# Special theme content
