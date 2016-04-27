7483915a1540c2015540e77f6b678a0da117c470
Shauna Gordon <shauna@shaunagordon.com>
2016-04-27 10:12:03 -0400

    Refining front page display.

--- publishconf.py
+++ publishconf.py
@@ -47,6 +47,7 @@ CATEGORY_SAVE_AS = ''
 AUTHOR_SAVE_AS = ''
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, 'extra/travis.yml': {'path': '.travis.yml'}}
 JINJA_FILTERS = { 'sidebar': filters.sidebar }
+SUMMARY_MAX_LENGTH = 1000
 
 # Blogroll
 LINKS = ()
